207f482aa1ec598bc79ed9cc7b06547e2b5f8651
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-01-27 13:34:00 -0500

    CSS list Characrer

--- src/Components/Episodes/Episode/Episode.jsx
+++ src/Components/Episodes/Episode/Episode.jsx
@@ -3,8 +3,9 @@ import CharacterCard from "../../Characters/Character/character";
 import './episode.css';
 
 export function Episode(props) {
-    const { name, airDate, episode, charactersList } = props
+    const { name, airDate, episode, charactersList, key } = props
     const [character, setCharacter] = useState([])
+    const [display, setDisplay] = useState(["none"])
 
     const apiMap = async (url) => {
       url.map(async (items) => {
@@ -14,18 +15,27 @@ export function Episode(props) {
       });
     };
 
+    const onShow= ()=>{
+      if(display==="none"){
+      display.slice(0);
+      setDisplay("flex")}
+      else{display.slice(0);
+        setDisplay("none")}
+    }
+
     useEffect(()=>{
       apiMap(charactersList)
     }, [charactersList]);
 
       return (
         <div className="episodeCard">
-          <div className="episodeInfo">
-            <h1>{name}</h1>
+          <div className="episodeHeadInfo">
+            <div className="episodeInfo"><h1>{name}</h1>
             <h3>{episode}</h3>
-            <p>{airDate}</p>
+            <p>{airDate}</p></div>
+            <button onClick={onShow}>Characters List</button>
           </div>
-          <div className="episodeCharacterCards">
+          <div className="episodeCharacterCards" id={"episodeCharacterCards"+key} style={{display:display}}>
                 {character.map((item) => (
                   <CharacterCard 
                   name={item.name}
@@ -35,6 +45,7 @@ export function Episode(props) {
                   gender={item.gender}
                   status={item.status}/>
                 ))}
+                <button onClick={onShow}>Characters List</button>
             </div>
         </div>
       );
